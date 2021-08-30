a9496ffd2963fc7bd0cae8aa7df109cc9fdfa8dd
Mateo Ledesma <mateo.14.ledesma@gmail.com>
2021-08-30 16:24:40 -0300

    change to nodejs

--- models/paymentMethod.go
+++
@@ -1,6 +0,0 @@
-package models
-
-type PaymentMethod struct {
-	ID   int    `json:"id"`
-	Name string `json:"name,omitempty"`
-}
--- services/services.go
+++
@@ -1,6 +0,0 @@
-package services
-
-type Services struct {
-	UsrSvc  IUsersService
-	SettSvc ISettingsService
-}
