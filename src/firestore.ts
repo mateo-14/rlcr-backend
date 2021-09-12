436e12c0be0180bca7a46cc9caef977e75352878
Mateo Ledesma <mateo.14.ledesma@gmail.com>
2021-09-12 00:55:06 -0300

    fix private_key parse

--- src/firestore.ts
+++ src/firestore.ts
@@ -4,7 +4,7 @@ const options = {
   type: process.env.FIREBASE_type,
   projectId: process.env.FIREBASE_project_id,
   privateKeyId: process.env.FIREBASE_private_key_id,
-  privateKey: process.env.FIREBASE_private_key,
+  privateKey: process.env.FIREBASE_private_key?.replace(/\\n/g, '\n'),
   clientEmail: process.env.FIREBASE_client_email,
   clientId: process.env.FIREBASE_client_id,
   authUri: process.env.FIREBASE_auth_uri,
