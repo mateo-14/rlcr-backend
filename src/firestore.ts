b697c66165aa19ac2c52515effc9c20be7c25f6d
Mateo Ledesma <mateo.14.ledesma@gmail.com>
2021-09-12 00:16:27 -0300

    change firestore json to ENV vars

--- src/firestore.ts
+++ src/firestore.ts
@@ -1,17 +1,16 @@
 import admin from 'firebase-admin';
-import serviceAccount from '../serviceAccount.json';
 
 const options = {
-  type: serviceAccount.type,
-  projectId: serviceAccount.project_id,
-  privateKeyId: serviceAccount.private_key_id,
-  privateKey: serviceAccount.private_key,
-  clientEmail: serviceAccount.client_email,
-  clientId: serviceAccount.client_id,
-  authUri: serviceAccount.auth_uri,
-  tokenUri: serviceAccount.token_uri,
-  authProviderX509CertUrl: serviceAccount.auth_provider_x509_cert_url,
-  clientC509CertUrl: serviceAccount.client_x509_cert_url,
+  type: process.env.FIREBASE_type,
+  projectId: process.env.FIREBASE_project_id,
+  privateKeyId: process.env.FIREBASE_private_key_id,
+  privateKey: process.env.FIREBASE_private_key,
+  clientEmail: process.env.FIREBASE_client_email,
+  clientId: process.env.FIREBASE_client_id,
+  authUri: process.env.FIREBASE_auth_uri,
+  tokenUri: process.env.FIREBASE_token_uri,
+  authProviderX509CertUrl: process.env.FIREBASE_auth_provider_x509_cert_url,
+  clientC509CertUrl: process.env.FIREBASE_client_x509_cert_url,
 };
 
 admin.initializeApp({
